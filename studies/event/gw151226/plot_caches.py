4c03f2aef146614c61e4bb90d7f0f3c1f0feb507
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2022-08-09 14:17:10 +1000

    remove junk

--- studies/event/gw151226/plot_caches.py
+++
@@ -1,7 +0,0 @@
-from deep_gw_pe_followup.restricted_prior import RestrictedPrior
-
-if __name__ == "__main__":
-    highq_prior = RestrictedPrior(filename="priors/lvk_restricted.prior")
-    highq_prior.plot_cache()
-    ias_prior = RestrictedPrior(filename="priors/ias_restricted.prior")
-    ias_prior.plot_cache()
--- studies/event/gw151226_ias_points_no_m1m2_reweighting/README.md
+++ studies/event/gw151226_ias_points_no_m1m2_reweighting/README.md
@@ -2,3 +2,4 @@
 1. `parallel_bilby_generation <ini>`
 2. `preprocess_prior outdir_<>/data/<label>_prior.json`
 3. `parallel_bilby_analysis <stuff>`
+4. If m1m2 constraints --> Compute "k" m1m2 normalisation 
