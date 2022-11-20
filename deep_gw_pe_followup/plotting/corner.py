d48857db248710247b2801a5ae6829f6b02c50bd
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2022-11-20 11:47:41 +1100

    add plotting utils

--- deep_gw_pe_followup/plotting/corner.py
+++ deep_gw_pe_followup/plotting/corner.py
@@ -1,21 +1,36 @@
-from corner import corner
+
 import numpy as np
 
+import matplotlib
+import bilby
+
+from collections import namedtuple
+
+import corner
+from corner.core import overplot_lines, overplot_points
+import matplotlib.lines as mlines
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import logging
+
+
 CORNER_KWARGS = dict(
+    bins=50,
     smooth=0.9,
     label_kwargs=dict(fontsize=30),
     title_kwargs=dict(fontsize=16),
-    color="tab:blue",
-    truth_color="tab:orange",
+    truth_color="black",
     quantiles=[0.16, 0.84],
-    levels=(1 - np.exp(-0.5), 1 - np.exp(-2), 1 - np.exp(-9.0 / 2.0)),
+    levels=(1 - np.exp(-0.5), 1 - np.exp(-2), 1 - np.exp(-9 / 2.0)),
     plot_density=False,
     plot_datapoints=False,
     fill_contours=True,
     max_n_ticks=3,
     verbose=False,
-    use_math_text=True,
+    use_math_text=False,
 )
+
 LABELS = dict(
     q=r"$q$",
     xeff=r"$\chi_{\rm eff}$",
@@ -24,9 +39,223 @@ LABELS = dict(
     cos_tilt_1=r"$\cos \theta_1$",
     cos_tilt_2=r"$\cos \theta_2$",
 )
+PARAMS = dict(
+    chi_eff=dict(latex_label=r"$\chi_{\rm eff}$", range=(-1, 1)),
+    chi_p=dict(latex_label=r"$\chi_{p}$", range=(0, 1)),
+    cos_tilt_1=dict(latex_label=r"$\cos\ \theta_1$", range=(-1, 1)),
+    cos_tilt_2=dict(latex_label=r"$\cos\theta_2$", range=(-1, 1)),
+    cos_theta_12=dict(latex_label=r"$\cos\ \theta_{12}$", range=(-1, 1)),
+    phi_12=dict(latex_label=r"$\phi_{12}$", range=(0, np.pi * 2)),
+    tilt_1=dict(latex_label=r"$tilt_{1}$", range=(0, np.pi)),
+    remnant_kick_mag=dict(latex_label=r"$|\vec{v}_k|\ $km/s", range=(0, 3000)),
+    chirp_mass=dict(latex_label="$M_{c}$", range=(5, 200)),
+    mass_1_source=dict(latex_label="$m_1^{\\mathrm{source}}$", range=(0, 200)),
+    mass_2_source=dict(latex_label="$m_2^{\\mathrm{source}}$", range=(0, 200)),
+    luminosity_distance=dict(latex_label="$d_L$", range=(50, 20000)),
+    log_snr=dict(latex_label="$\\rm{log}_{10}\ \\rho$)", range=(-1, 3)),
+    snr=dict(latex_label="$\mathrm{SNR}$", range=(0, 20)),
+    log_likelihood=dict(latex_label="$\log\mathrm{L}$", range=(40, 100)),
+    mass_ratio=dict(latex_label="$q$", range=(0, 1)),
+)
 
 
 def plot_corner(df, fname="corner.png"):
     labels = [LABELS.get(i, i.replace("_", "")) for i in df.columns.values]
-    fig = corner(df, labels=labels, **CORNER_KWARGS)
+    fig = corner.corner(df, labels=labels, **CORNER_KWARGS)
     fig.savefig(fname)
+
+
+
+matplotlib.rcParams['axes.prop_cycle'] = matplotlib.cycler(
+    color=["k", "tab:orange", "tab:blue", "tab:green", "tab:purple"])
+
+logger = logging.getLogger()
+
+
+
+
+
+def _get_one_dimensional_median_and_error_bar(
+        posterior, key, fmt=".2f", quantiles=(0.16, 0.84)
+):
+    """Calculate the median and error bar for a given key
+
+    Parameters
+    ----------
+    key: str
+        The parameter key for which to calculate the median and error bar
+    fmt: str, ('.2f')
+        A format string
+    quantiles: list, tuple
+        A length-2 tuple of the lower and upper-quantiles to calculate
+        the errors bars for.
+
+    Returns
+    -------
+    summary: namedtuple
+        An object with attributes, median, lower, upper and string
+
+    """
+    summary = namedtuple("summary", ["median", "lower", "upper", "string"])
+
+    if len(quantiles) != 2:
+        raise ValueError("quantiles must be of length 2")
+
+    quants_to_compute = np.array([quantiles[0], 0.5, quantiles[1]])
+    quants = np.percentile(posterior[key], quants_to_compute * 100)
+    summary.median = quants[1]
+    summary.plus = quants[2] - summary.median
+    summary.minus = summary.median - quants[0]
+
+    fmt = "{{0:{0}}}".format(fmt).format
+    string_template = r"${{{0}}}_{{-{1}}}^{{+{2}}}$"
+    summary.string = string_template.format(
+        fmt(summary.median), fmt(summary.minus), fmt(summary.plus)
+    )
+    return summary
+
+
+def _add_ci_vals_to_marginalised_posteriors(
+        fig, params, posterior: pd.DataFrame
+):
+    # plt the quantiles
+    axes = fig.get_axes()
+    for i, par in enumerate(params):
+        ax = axes[i + i * len(params)]
+        if ax.title.get_text() == "":
+            ax.set_title(
+                _get_one_dimensional_median_and_error_bar(
+                    posterior, par, quantiles=CORNER_KWARGS["quantiles"]
+                ).string,
+                **CORNER_KWARGS["title_kwargs"],
+            )
+
+
+def overlaid_corner(
+        samples_list,
+        params,
+        samples_colors,
+        sample_labels=[],
+        fname="",
+        title=None,
+        truths=None,
+        ranges=[],
+        quants=False,
+        override_kwargs={}
+):
+    """Plots multiple corners on top of each other
+
+    :param samples_list: list of all posteriors to be plotted ontop of each other
+    :type samples_list: List[pd.DataFrame]
+    :param sample_labels: posterior's labels to be put on legend
+    :type sample_labels: List[str]
+    :param params: posterior params names (used to access posteriors samples)
+    :type params: List[str]
+    :param samples_colors: Color for each posterior
+    :type samples_colors: List[Color]
+    :param fname: Plot's save path
+    :type fname: str
+    :param title: Plot's suptitle if not None
+    :type title: None/str
+    :param truths: posterior param true vals
+    :type truths: Dict[str:float]
+    :return: None
+    """
+    logger.info(f"Plotting {fname}")
+    logger.info(f"Cols in samples: {samples_list[0].columns.values}")
+    # sort the sample columns
+
+    samples_list = [s[params].sample(1500) for s in samples_list]
+    base_s = samples_list[0]
+
+    # get plot_posterior_predictive_check range, latex labels, colors and truths
+    plot_range, axis_labels = [], []
+    for p in params:
+        p_data = PARAMS.get(
+            p,
+            dict(range=(min(base_s[p]), max(base_s[p])), latex_label=f"${p}$"),
+        )
+        plot_range.append(p_data["range"])
+        axis_labels.append(p_data["latex_label"])
+
+    if isinstance(ranges, list):
+        if len(ranges) != 0:
+            plot_range = ranges
+    elif isinstance(ranges, type(None)):
+        plot_range = None
+
+    # get some constants
+    n = len(samples_list)
+    _, ndim = samples_list[0].shape
+    min_len = min([len(s) for s in samples_list])
+
+    c_kwargs = CORNER_KWARGS.copy()
+    c_kwargs.update(
+        range=plot_range,
+        labels=axis_labels,
+        truths=truths,
+        truth_color=CORNER_KWARGS["truth_color"]
+    )
+
+    for k, v in override_kwargs.items():
+        c_kwargs[k] = v
+
+    hist_kwargs = dict(lw=3, histtype='step', alpha=0.5)
+    if not quants:
+        c_kwargs.pop("quantiles", None)
+
+    fig = corner.corner(
+        samples_list[0],
+        color=samples_colors[0],
+        **c_kwargs,
+        hist_kwargs=dict(fc=samples_colors[0], ec=samples_colors[0], **hist_kwargs)
+    )
+
+    for idx in range(1, n):
+        col = samples_colors[idx]
+        fig = corner.corner(
+            samples_list[idx],
+            fig=fig,
+            weights=_get_normalisation_weight(len(samples_list[idx]), min_len),
+            color=col,
+            **c_kwargs,
+            hist_kwargs=dict(fc=col, ec=col, **hist_kwargs)
+        )
+
+    if len(samples_list) == 1:
+        _add_ci_vals_to_marginalised_posteriors(fig, params, samples_list[0])
+
+    if len(sample_labels) > 1:
+        plt.legend(
+            handles=[
+                mlines.Line2D(
+                    [], [], color=samples_colors[i], label=sample_labels[i]
+                )
+                for i in range(len(sample_labels))
+            ],
+            fontsize=18,
+            frameon=False,
+            bbox_to_anchor=(1, ndim),
+            loc="upper right",
+        )
+    if title:
+        fig.suptitle(title, y=0.97)
+        fig.subplots_adjust(top=0.75)
+    if fname:
+        fig.savefig(fname)
+        plt.close(fig)
+    else:
+        return fig
+
+
+def _get_normalisation_weight(len_current_samples, len_of_longest_samples):
+    return np.ones(len_current_samples) * (
+            len_of_longest_samples / len_current_samples
+    )
+
+
+def overplot_sample(fig, data, color):
+    overplot_lines(fig, data, color=color)
+    overplot_points(fig, [data, data], color=color)
+
+
--- studies/literature_gw151226_results/plot_literature_qxeff.py
+++ studies/literature_gw151226_results/plot_literature_qxeff.py
@@ -6,7 +6,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.10.3
+#       jupytext_version: 1.14.1
 #   kernelspec:
 #     display_name: tess
 #     language: python
@@ -22,8 +22,40 @@ import pandas as pd
 from matplotlib.colors import LinearSegmentedColormap, colorConverter
 import seaborn as sns
 
+plt.style.use("https://gist.githubusercontent.com/avivajpeyi/4d9839b1ceb7d3651cbb469bc6b0d69b/raw/55fab35b1c811dc3c0fdc6d169d67607e2c3edfc/publication.mplstyle")
+plt.rcParams.update({
+    'axes.grid': False,
+    'font.size':20,
+    'axes.labelsize': 30,   
+    'ytick.labelsize': 20,  
+    'xtick.labelsize': 20,    
+})
+
+ 
+
 SIGMA_LEVELS = [1 - np.exp(-0.5), 1 - np.exp(-2), 1 - np.exp(-9 / 2.0)]
 SIGMA_LEVELS = [SIGMA_LEVELS[0], SIGMA_LEVELS[-1]] # 3 and 1-sigma levels
+
+
+
+def get_median_and_err(x):
+    quants_to_compute = np.array([0.16, 0.5, 0.84])
+    quants = np.percentile(x, quants_to_compute * 100)
+    median = quants[1]
+    plus, minus = quants[2] - median, median - quants[0]
+    return median, plus, minus
+    
+
+def get_str_for_val(x):
+    median, plus, minus = get_median_and_err(x)
+    fmt='.2f'
+    fmt = "{{0:{0}}}".format(fmt).format
+    string_template = r"${{{0}}}_{{-{1}}}^{{+{2}}}$"
+    string = string_template.format(
+        fmt(median), fmt(minus), fmt(plus))
+    return string
+
+
 
 
 def load_qxeff_data(fn):
@@ -32,7 +64,7 @@ def load_qxeff_data(fn):
 
 
 
-def format_cntr_data(x,y, levels, smooth=1.2, bins=75, range=[[0,1],[0,0.6]]):
+def format_cntr_data(x,y, levels, smooth=1.5, bins=100, range=[[0.05,1],[0,0.6]]):
     H, X, Y = np.histogram2d(
         x.flatten(),
         y.flatten(),
@@ -104,11 +136,16 @@ def format_cntr_data(x,y, levels, smooth=1.2, bins=75, range=[[0,1],[0,0.6]]):
     return X2, Y2, H2, V, H
 
 
-def add_cntr(ax, x,y, color, label, levels=SIGMA_LEVELS):
-    X2, Y2, H2, V, H = format_cntr_data(x,y, levels=levels)
+def add_cntr(ax, x,y, color, label, levels=SIGMA_LEVELS, labelypos=0, bin_range=[[0.05,1],[0,0.6]]):
     cmap = sns.color_palette(color, as_cmap=True)
-    c = cmap(levels[-1])
+    c = cmap(0.75)
+    
+    # LINE
+    X2, Y2, H2, V, H = format_cntr_data(x,y, levels=[levels[-1]], range=bin_range)
     ax.contour(X2, Y2, H2.T, V, colors=[c] * len(levels), alpha=0.25, antialiased=True)
+    
+    # FILL
+    X2, Y2, H2, V, H = format_cntr_data(x,y, levels=levels, range=bin_range)
     ax.contourf(
         X2,
         Y2,
@@ -127,24 +164,58 @@ def add_cntr(ax, x,y, color, label, levels=SIGMA_LEVELS):
             antialiased=False,
         alpha=0.25
         )
-    ax.plot(np.median(x), np.median(y), color=c, zorder=100, label=label)
-    ax.scatter(np.median(x), np.median(y), color=c, zorder=100)
+    ax.plot(np.median(x), np.median(y), color=c, zorder=100, alpha=0.5, label=label)
+    med_x, xup, xlow = get_median_and_err(x)
+    med_y, yup, ylow = get_median_and_err(y)
+    # ax.errorbar(med_x, med_y, xerr=[[xlow], [xup]],  yerr=[[ylow], [yup]], color=c, zorder=100, alpha=0.1)
+    ax.scatter(med_x, med_y, color=c, zorder=100, alpha=1)
+    print(f"{label}: q {median_val_and_1_sig_err(x)}, xeff {get_str_for_val(y)}")
+    add_txt_marker(ax, med_x, med_y, c, label, labelypos)
+    
+
+def add_txt_marker(ax, x, y, color, letter, ypos=0):
+    kwgs = dict(color=color, zorder=100)
+    ax.scatter(x, y, marker=".", s=10, **kwgs)
+    ax.annotate(letter, xy=(x, y), xycoords='data',
+                xytext=(5, ypos), textcoords='offset points', **kwgs)
 
 
-def main():
+    
+def make_literature_comparison_plot(with_old_pts=False, fname="literature_qxeff.png"):
     NITZ_QXEFF = load_qxeff_data("nitz_qxeff.npz")
     MATEU_QXEFF = load_qxeff_data("mateu_qxeff.npz")
     CHIA_QXEFF = load_qxeff_data("chia_qxeff.npz")
+
+
     
     fig, ax = plt.subplots(1,1, figsize=(5,5))
-    add_cntr(ax, NITZ_QXEFF['q'].values, NITZ_QXEFF['xeff'].values, "Oranges", "Nitz+")
-    add_cntr(ax, MATEU_QXEFF['q'].values, MATEU_QXEFF['xeff'].values, "Greens", "Mateu+")
-    add_cntr(ax, CHIA_QXEFF['q'].values, CHIA_QXEFF['xeff'].values, "Purples", "Chia+")
+    
+    
+    if with_old_pts:
+        PT_A = [0.68, 0.15]
+        PT_B = [0.15, 0.5]
+        add_txt_marker(ax, *PT_A, color="gray", letter="A", ypos=-15)
+        add_txt_marker(ax, *PT_B, color="gray", letter="B", ypos=0)
+
+    add_cntr(ax, NITZ_QXEFF['q'].values, NITZ_QXEFF['xeff'].values, "Reds", "Nitz+", labelypos=5)
+    add_cntr(ax, MATEU_QXEFF['q'].values, MATEU_QXEFF['xeff'].values, "Greens", "ML+", labelypos=-15)
+    add_cntr(ax, CHIA_QXEFF['q'].values, CHIA_QXEFF['xeff'].values, "Blues", "Chia+", labelypos=5)
     ax.set_xlim(0, 1.0)
+    ax.set_ylim(0, 0.6)
+    ax.set_xticks([-0, 0.3, 0.5, 1])
+    ax.set_yticks([0.1, 0.2, 0.3, 0.5])
+    
+    
+    plt.minorticks_off()
     ax.set_xlabel(r"$q$")
     ax.set_ylabel(r"$\chi_{\rm eff}$")
-    l = ax.legend()
-    fig.savefig("literature_qxeff.png", dpi=500)
+    plt.tight_layout()
+    fig.savefig(fname, dpi=500)
+    
+    
+def main():
+    make_literature_comparison_plot()
+    make_literature_comparison_plot(True, "comparison_with_old.png")
     
 if __name__ == "__main__":
     main()
